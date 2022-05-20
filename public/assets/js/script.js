697d8c223871ffa21e8c91d49e68f6af5ee71111
Dimitri Stroe <stroe.dimitri@gmail.com>
2022-05-20 11:09:07 +0200

    footerFixed

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,7 @@ const body = document.body;
 const scrollUp = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
-
+body.classList.add(scrollDown)
 window.addEventListener('scroll',()=>{
     const currentScroll = window.scrollY;
     if (currentScroll<=0){
@@ -11,7 +11,7 @@ window.addEventListener('scroll',()=>{
     }
 
 
-if (currentScroll >=lastScroll && !body.classList.contains(scrollUp)){
+if (currentScroll >lastScroll && !body.classList.contains(scrollUp)){
     body.classList.remove(scrollDown);
     body.classList.add(scrollUp);
 }else if (
