2c6df4901112462654ebc18c1d3a45e7790a23a3
Stephane SWAENEPOEL <102791292+Zokkyen@users.noreply.github.com>
2022-05-20 11:09:28 +0200

    Mise en forme des fichiers

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,13 +3,9 @@ const scrollUp = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
 
+/* Fonction d'ajout du footer quand scroll bas et l'enlever quand scroll haut */
 window.addEventListener('scroll', () => {
     const currentScroll = window.scrollY;
-    if (currentScroll <= 0) {
-        // body.classList.remove(scrollDown);
-        return;
-    }
-
     if (currentScroll >= lastScroll && !body.classList.contains(scrollUp)) {
         body.classList.remove(scrollDown);
         body.classList.add(scrollUp);
