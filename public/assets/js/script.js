171d52df113db68839caa65c43cafa6a86c0d430
Stephane SWAENEPOEL <102791292+Zokkyen@users.noreply.github.com>
2022-05-20 10:47:23 +0200

    Maj scroll footer

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,23 +3,22 @@ const scrollUp = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     const currentScroll = window.scrollY;
-    if (currentScroll<=0){
+    if (currentScroll <= 0) {
         // body.classList.remove(scrollDown);
         return;
     }
 
-
-if (currentScroll >=lastScroll && !body.classList.contains(scrollUp)){
-    body.classList.remove(scrollDown);
-    body.classList.add(scrollUp);
-}else if (
-    currentScroll < lastScroll &&
-    body.classList.contains(scrollUp)
-  ) {
-    body.classList.remove(scrollUp);
-    body.classList.add(scrollDown);
-  }
-  lastScroll = currentScroll;
+    if (currentScroll >= lastScroll && !body.classList.contains(scrollUp)) {
+        body.classList.remove(scrollDown);
+        body.classList.add(scrollUp);
+    } else if (
+        currentScroll < lastScroll &&
+        body.classList.contains(scrollUp)
+    ) {
+        body.classList.remove(scrollUp);
+        body.classList.add(scrollDown);
+    }
+    lastScroll = currentScroll;
 });
